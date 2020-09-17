b1c18c7fd9d1f665caab15705ddb3185d57d0fad
ABHISHEK MISHRA <51957554+Abhishekmishra-17@users.noreply.github.com>
2020-09-17 17:27:03 +0530

    Update message.py

--- proakm4/message.py
+++ proakm4/message.py
@@ -4,15 +4,15 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACbaf4d39ddf69801f4255590968c564b8'
-auth_token = 'a9b1ac8c0d10834190e18340473dc9df'
+account_sid = 'ACbaf4d39ddf69801f4255590968c564b8ak'
+auth_token = 'a9b1ac8c0d10834190e18340473dc9dfm'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
                      body="Join Earth's mightiest heroes. Like Kevin Bacon.",
                      from_='+14012576091',
-                     to='+918874036548'
+                     to='+91registermobilenumber'
                  )
 
 print(message.sid)
