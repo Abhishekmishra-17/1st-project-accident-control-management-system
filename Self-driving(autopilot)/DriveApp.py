621e4f95527bfe46f3fecc0b41aaf62d7be7e599
ABHISHEK MISHRA <51957554+Abhishekmishra-17@users.noreply.github.com>
2021-07-07 23:28:46 +0530

    Add files via upload

---
+++ Self-driving(autopilot)/DriveApp.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2
+from keras.models import load_model
+from urllib.request import urlopen
+model = load_model('Autopilot.h5')
+
+def keras_predict(model, image):
+    processed = keras_process_image(image)
+    steering_angle = float(model.predict(processed, batch_size=1))
+    steering_angle = steering_angle * 100
+    return steering_angle
+
+
+def keras_process_image(img):
+    image_x = 40
+    image_y = 40
+    img = cv2.resize(img, (image_x, image_y))
+    img = np.array(img, dtype=np.float32)
+    img = np.reshape(img, (-1, image_x, image_y, 1))
+    return img
+
+
+steer = cv2.imread('images.png', 0)
+rows, cols = steer.shape
+smoothed_angle = 0
+url="http://192.168.43.1:8080/shot.jpg"
+while (True):
+    imgResp=urlopen(url)
+    imgNp=np.array(bytearray(imgResp.read()),dtype=np.uint8)
+    frame=cv2.imdecode(imgNp,-1)
+    gray = cv2.resize((cv2.cvtColor(frame, cv2.COLOR_RGB2HSV))[:, :, 1], (40, 40))
+    steering_angle = keras_predict(model, gray)
+    print(steering_angle)
+    cv2.imshow('frame', cv2.resize(frame, (500, 300), interpolation=cv2.INTER_AREA))
+    smoothed_angle += 0.2 * pow(abs((steering_angle - smoothed_angle)), 2.0 / 3.0) * (
+        steering_angle - smoothed_angle) / abs(
+        steering_angle - smoothed_angle)
+    M = cv2.getRotationMatrix2D((cols / 2, rows / 2), -smoothed_angle, 1)
+    dst = cv2.warpAffine(steer, M, (cols, rows))
+    cv2.imshow("steering wheel", dst)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows()
